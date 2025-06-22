e94c25509ff9e15d4281a0cdbb6d3d2cee460ce6
El Madkour Yassine <elmadkouryassin@gmail.com>
2025-06-22 02:05:31 +0100

    Initial commit: SMS Gateway web interface

--- app.py
+++ app.py
@@ -41,4 +41,4 @@ def home():
     return render_template('dashboard.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='192.168.88.132', port=5000, debug=True)
