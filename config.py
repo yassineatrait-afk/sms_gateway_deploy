bf1f1f31df5652072652b4854f0b4e8ee2cd860c
yassineatrait-afk <yassine.atrait@gmail.com>
2025-09-07 16:11:30 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config:
     # notez l'ajout de "+pymysql" et l'encodage du '@' dans le mot de passe (%40)
     SQLALCHEMY_DATABASE_URI = (
-      "mysql+pymysql://sms_user:Estem212al%40@localhost/sms_gateway"
+      "mysql+pymysql://sms_user:PASSWORD%40@localhost/sms_gateway"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
